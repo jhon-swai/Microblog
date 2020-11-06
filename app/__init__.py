bef678cb8e8cec86868c451cc1194fcd4b008f8f
jhon-swai <jhonswai@gmail.com>
2020-11-06 18:48:07 +0300

    login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # configuration for the secret key from config class
@@ -9,6 +10,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# login manager 
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
 
